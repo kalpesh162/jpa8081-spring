1639dedfdc37c7e634a81ade55529a20bd54172e
kalpesh <kalpesh162@gmail.com>
2025-11-29 10:50:46 +0530

    IOC

---
+++ SpringHelloWorld/src/com/app/App.java
@@ -0,0 +1,36 @@
+package com.app;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.bean.Employee;
+
+public class App {
+
+	public static void main(String[] args) {
+		// BeanFactory
+		// ApplicationContext
+
+		Resource resource = new ClassPathResource("spring.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext("spring.xml");
+		/*
+		 * Employee employee = (Employee) context.getBean("employee");
+		 * 
+		 * System.out.println(employee);
+		 */
+
+		Employee employee = (Employee) factory.getBean("employee");
+		System.out.println(employee);
+		System.out.println("******");
+		Employee employee1 = (Employee) factory.getBean("employee1");
+		System.out.println(employee1);
+		System.out.println("******");
+
+	}
+
+}
---
+++ SpringHelloWorld/src/com/bean/Employee.java
@@ -0,0 +1,55 @@
+package com.bean;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+	private int id;
+	private String name;
+	private double salary;
+
+	public Employee() {
+		System.out.println("Default Constructor");
+	}
+
+	public Employee(int id, String name, double salary) {
+		System.out.println("Paremeterized Constructor");
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		System.out.println("Set Id");
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("Set Name");
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		System.out.println("Set Salary");
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+
+}
